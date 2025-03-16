8868cab59abdddcc3facc5bb672a2038331389ea
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-03-16 16:49:53 +0700

    branch footer revision

--- src/components/home/Section5.js
+++ src/components/home/Section5.js
@@ -1,128 +1,119 @@
 import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPhone, faEnvelope, faMapMarkerAlt, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
-import ev_pic from "@/../docs/img/ev.svg";
-import Call from "../../assets/svgs/Call";
+import EvImage from "@/assets/images/ev.svg";
+import Call from "@/assets/svgs/Call";
 import Mail from "@/assets/svgs/Mail";
 import Insta from "@/assets/svgs/Insta";
 import Map from "@/assets/svgs/Map";
 import Send from "@/assets/svgs/Send";
 
 export default function Section5() {
-  return (
+	return (
+		<section className="relative flex flex-col items-center justify-center min-h-screen px-0 py-10 overflow-hidden">
+			<div className="absolute left-0 top-1/2 -translate-y-1/2 w-[311px] h-[311px] bg-[radial-gradient(circle,_rgba(68,194,161,0.5)_0%,_transparent_90%)] blur-3xl hidden sm:block"></div>
 
-<section className="relative flex flex-col items-center justify-center min-h-screen px-0 py-10 overflow-hidden">
+			<div className="absolute right-0 top-1/3 -translate-y-1/2 w-[311px] h-[311px] md:bg-[radial-gradient(circle,_rgba(68,194,161,0.5)_0%,_transparent_90%)] blur-3xl hidden sm:block"></div>
 
-  <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[311px] h-[311px] bg-[radial-gradient(circle,_rgba(68,194,161,0.5)_0%,_transparent_90%)] blur-3xl hidden sm:block"></div>
+			<div className="text-center mb-6">
+				<h1 className="text-xl font-medium">
+					<span className="text-transparent bg-clip-text bg-gradient-to-r to-[#121212] from-primary-n1">Reach Us</span>
+				</h1>
+				<h1 className="text-2xl md:text-4xl lg:text-5xl font-bold mb-2">
+					GET IN <span className="text-primary-n1">TOUCH</span>
+				</h1>
+				<p className="text-xs md:text-lg lg:text-xl text-foreground-2 mb-2">
+					Mari berkolaborasi dan ciptakan sesuatu yang luar biasa.
+					<br />
+					Ada saran atau masukan? Jangan ragu untuk menghubungi kami.
+				</p>
+			</div>
 
-  <div className="absolute right-0 top-1/3 -translate-y-1/2 w-[311px] h-[311px] md:bg-[radial-gradient(circle,_rgba(68,194,161,0.5)_0%,_transparent_90%)] blur-3xl hidden sm:block"></div>
+			<div className="max-w-5xl mx-auto">
+				<div className="bg-white shadow-lg rounded-3xl sm:rounded-4xl overflow-hidden grid grid-cols-1 md:grid-cols-[65%_35%] pt-6">
+					<div className="p-6 md:p-8lg:p-10 bg-white">
+						<h2 className="text-xl font-bold text-gray-800">Kirimkan Pesan</h2>
+						<form className="mt-4">
+							<div className="flex flex-col md:flex-row gap-4 mb-3">
+								<input
+									type="text"
+									name="first_name"
+									className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
+									placeholder="Nama Depan"
+									required
+								/>
+								<input
+									type="text"
+									name="last_name"
+									className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
+									placeholder="Nama Belakang"
+								/>
+							</div>
 
-  <div className="text-center mb-6">
-        <h1 className="text-xl font-medium">
-          <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#121212] from-primary-n1">
-            Reach Us
-          </span>
-        </h1>
-        <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold mb-2">
-          GET IN <span className="text-primary-n1">TOUCH</span>
-        </h1>
-        <p className="text-xs md:text-lg lg:text-xl text-foreground-2 mb-2">
-          Mari berkolaborasi dan ciptakan sesuatu yang luar biasa.
-          <br />Ada saran atau masukan? Jangan ragu untuk menghubungi kami.
-        </p>
-      </div>
+							<div className="flex flex-col md:flex-row gap-4 mb-3">
+								<input
+									type="email"
+									name="email"
+									className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
+									placeholder="Email"
+									required
+								/>
+								<input
+									type="text"
+									name="phone"
+									className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
+									placeholder="No. Telp"
+									required
+								/>
+							</div>
 
-      <div className="max-w-5xl mx-auto">
-  		<div className="bg-white shadow-lg rounded-3xl sm:rounded-4xl overflow-hidden grid grid-cols-1 md:grid-cols-[65%_35%] pt-6">
+							<div className="mb-3">
+								<textarea
+									name="message"
+									className="w-full px-0 py-2 text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
+									placeholder="Apa yang ingin anda sampaikan?"
+								></textarea>
+							</div>
 
-          <div className="p-6 md:p-8lg:p-10 bg-white">
-            <h2 className="text-xl font-bold text-gray-800">Kirimkan Pesan</h2>
-            <form className="mt-4">
-              
-              <div className="flex flex-col md:flex-row gap-4 mb-3">
-                <input
-                  type="text"
-                  name="first_name"
-                  className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
-                  placeholder="Nama Depan"
-                  required
-                />
-                <input
-                  type="text"
-                  name="last_name"
-                  className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
-                  placeholder="Nama Belakang"
-                />
-              </div>
+							<div className="flex justify-end">
+								<button className="bg-primary-n1 text-white px-4 py-2 rounded-lg flex items-center space-x-2 hover:bg-[#3bb99a]">
+									<Send className="fill-white" />
+									<span>Kirim</span>
+								</button>
+							</div>
+						</form>
+					</div>
 
-              <div className="flex flex-col md:flex-row gap-4 mb-3">
-                <input
-                  type="email"
-                  name="email"
-                  className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
-                  placeholder="Email"
-                  required
-                />
-                <input
-                  type="text"
-                  name="phone"
-                  className="w-full px-0 py-2.5 text-md text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
-                  placeholder="No. Telp"
-                  required
-                />
-              </div>
+					<div className="relative p-4 bg-[#44c2a1] bg-gradient-to-t from-primary-n1 text-white rounded-r-3xl flex flex-col justify-between">
+						<div>
+							<h3 className="text-xl font-semibold">Hubungi Kami</h3>
 
-              <div className="mb-3">
-                <textarea
-                  name="message"
-                  className="w-full px-0 py-2 text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
-                  placeholder="Apa yang ingin anda sampaikan?"
-                ></textarea>
-              </div>
+							<div className="mt-4 space-y-3">
+								<div className="flex items-center space-x-3">
+									<Call className="fill-white" />
+									<p className="text-sm">+62 851-4116-8042</p>
+								</div>
+								<div className="flex items-center space-x-3">
+									<Mail className="fill-white" />
+									<p className="text-sm">flyhighsinergi.idn@gmail.com</p>
+								</div>
+								<div className="flex items-center space-x-3">
+									<Insta className="fill-white" />
+									<p className="text-sm">flyhighcorp_</p>
+								</div>
+								<div className="flex items-center space-x-3">
+									<Map className="fill-white" />
+									<p className="text-sm">Klampis, Surabaya</p>
+								</div>
+							</div>
+						</div>
 
-              <div className="flex justify-end">
-                <button className="bg-primary-n1 text-white px-4 py-2 rounded-lg flex items-center space-x-2 hover:bg-[#3bb99a]">
-                  <Send className="fill-white"/>
-                  <span>Kirim</span>
-                </button>
-              </div>
-            </form>
-          </div>
-
-          <div className="relative p-6 bg-[#44c2a1] bg-gradient-to-t from-primary-n1 text-white rounded-r-3xl flex flex-col justify-between">
-            <div>
-              <h3 className="text-xl font-semibold">Hubungi Kami</h3>
-              
-              <div className="mt-4 space-y-3">
-                <div className="flex items-center space-x-3">
-                  <Call className="fill-white" />
-                  <p className="text-sm">+62 851-4116-8042</p>
-                </div>
-                <div className="flex items-center space-x-3">
-                  <Mail className="fill-white" />
-                  <p className="text-sm">flyhighsinergi.idn@gmail.com</p>
-                </div>
-                <div className="flex items-center space-x-3">
-                  <Insta className="fill-white" />
-                  <p className="text-sm">flyhighcorp_</p>
-                </div>
-                <div className="flex items-center space-x-3">
-                  <Map className="fill-white" />
-                  <p className="text-sm">Klampis, Surabaya</p>
-                </div>
-              </div>
-            </div>
-
-            <div className="absolute bottom-4 right-4 ">
-              <div className="w-15 md:w-36">
-              <Image src={ev_pic} alt="EV Logo" sizes="100%" className="w-full" />
-              </div>
-            </div>
-          </div>      
-        </div>
-      </div>
-</section>
-
-  );
+						<div className="absolute bottom-4 right-4 ">
+							<div className="w-15 md:w-36">
+								<Image src={EvImage} alt="EV Logo" sizes="100%" className="w-full" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
--- src/components/home/Section6.js
+++ src/components/home/Section6.js
@@ -1,77 +1,126 @@
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-config.autoAddCss = false;
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faTiktok } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-
-import Image from "next/image";
-import logo from "@/../docs/img/BETULIN.svg";
+import BetulinSvg from "@/assets/svgs/Betulin";
 import Insta from "@/assets/svgs/Insta";
 import Mail from "@/assets/svgs/Mail";
 import Tiktok from "@/assets/svgs/Tiktok";
 
 export default function Section6() {
 	return (
-	  <footer className="bg-primary-n1 text-white p-6 md:p-8">
-		<div className="container mx-auto grid grid-cols-1 md:grid-cols-[30%_70%] gap-6 md:gap-8">
-		  
-		  <div className="text-left mx-auto md:mx-4 lg:mx-20">
-			<Image src={logo} alt="Logo BETULIN" width={120} height={120} className="mx-0" />
-			<p className="text-md md:text-lg mt-2 cursor-default">
-			  Optimalkan performa kendaraan listrik anda dengan perpaduan berbagai fitur BETULIN.
-			</p>
-  
-			<div className="flex justify-left md:justify-start space-x-3 mt-4">
-			  <a href="#" className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition">
-				<Insta className="fill-white" />
-			  </a>
-			  <a href="#" className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition">
-				<Tiktok className="fill-white"/>
-			  </a>
-			  <a href="#" className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition">
-				<Mail className="fill-white" />
-			  </a>
-			</div>
-		  </div>
-  
-		  <div className="grid grid-cols-3 md:grid-cols-3 gap-6">
-			<div className="text-center md:text-left flex flex-col ml-0 md:ml-15 lg:ml-24">
-			  <h3 className="text-lg  md:text-xl mt font-semibold text-white cursor-default">About</h3>
-			  <ul className="mt-9 lg:mt-2 md:mt-1.5 space-y-2 md:text-base">
-				<li><a href="#" className="hover:text-white transition">Home</a></li>
-				<li><a href="#" className="hover:text-white transition">About Us</a></li>
-				<li><a href="#" className="hover:text-white transition">Services</a></li>
-				<li><a href="#" className="hover:text-white transition">Testimonials</a></li>
-			  </ul>
-			</div>
+		<footer className="bg-primary-n1 text-white p-6 md:p-8">
+			<div className="container mx-auto grid grid-cols-1 md:grid-cols-[30%_70%] gap-6 md:gap-8">
+				<div className="text-left mx-auto md:mx-4 lg:mx-20">
+					<BetulinSvg className="fill-white stroke-white" />
 
-			<div className="text-center md:text-left flex flex-col ">
-			  <h3 className="text-lg md:text-xl font-semibold text-white cursor-default">Our Services</h3>
-			  <ul className="mt-2 space-y-2 md:text-base">
-				<li><a href="#" className="hover:text-white transition">Bengkel</a></li>
-				<li><a href="#" className="hover:text-white transition">Marketplace Sparepart</a></li>
-				<li><a href="#" className="hover:text-white transition">Konsultasi AI dan Mekanik</a></li>
-				<li><a href="#" className="hover:text-white transition">Panggil Mekanik</a></li>
-				<li><a href="#" className="hover:text-white transition">Konten Edukasi EV</a></li>
-				<li><a href="#" className="hover:text-white transition">Pengingat Perawatan</a></li>
-			  </ul>
+					<p className="text-md md:text-lg mt-2 cursor-default">
+						Optimalkan performa kendaraan listrik anda dengan perpaduan berbagai fitur BETULIN.
+					</p>
+
+					<div className="flex justify-left md:justify-start space-x-3 mt-4">
+						<a
+							href="#"
+							className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition"
+						>
+							<Insta className="fill-white" />
+						</a>
+						<a
+							href="#"
+							className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition"
+						>
+							<Tiktok className="fill-white" />
+						</a>
+						<a
+							href="#"
+							className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition"
+						>
+							<Mail className="fill-white" />
+						</a>
+					</div>
+				</div>
+
+				<div className="grid grid-cols-3 md:grid-cols-3 gap-6">
+					<div className="text-center md:text-left flex flex-col ml-0 md:ml-15 lg:ml-24">
+						<h3 className="text-lg  md:text-xl mt font-semibold text-white cursor-default">About</h3>
+						<ul className="mt-9 lg:mt-2 md:mt-1.5 space-y-2 md:text-base">
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									About Us
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Services
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Testimonials
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div className="text-center md:text-left flex flex-col ">
+						<h3 className="text-lg md:text-xl font-semibold text-white cursor-default">Our Services</h3>
+						<ul className="mt-2 space-y-2 md:text-base">
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Bengkel
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Marketplace Sparepart
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Konsultasi AI dan Mekanik
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Panggil Mekanik
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Konten Edukasi EV
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Pengingat Perawatan
+								</a>
+							</li>
+						</ul>
+					</div>
+
+					<div className="text-center md:text-left flex flex-col">
+						<h3 className="text-lg md:text-xl font-semibold text-white cursor-default">Our Company</h3>
+						<ul className="mt-2 space-y-2 md:text-base">
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Blog
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Vision
+								</a>
+							</li>
+							<li>
+								<a href="#" className="hover:text-white transition">
+									Mission
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
 			</div>
-			
-			<div className="text-center md:text-left flex flex-col">
-			  <h3 className="text-lg md:text-xl font-semibold text-white cursor-default">Our Company</h3>
-			  <ul className="mt-2 space-y-2 md:text-base">
-				<li><a href="#" className="hover:text-white transition">Blog</a></li>
-				<li><a href="#" className="hover:text-white transition">Vision</a></li>
-				<li><a href="#" className="hover:text-white transition">Mission</a></li>
-			  </ul>
-			</div>
-  
-		  </div>
-  
-		</div>
-	  </footer>
+		</footer>
 	);
-  }
-  
+}
