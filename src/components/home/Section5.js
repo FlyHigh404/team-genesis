f56f4f7a1959bf3e867d3cfb39c2012b071f3931
renaramarosalinda <21081010190@student.upnjatim.ac.id>
2025-03-16 16:04:46 +0700

    add:section 5

--- src/components/home/Section5.js
+++ src/components/home/Section5.js
@@ -3,11 +3,22 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope, faMapMarkerAlt, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import ev_pic from "@/../docs/img/ev.svg";
+import Call from "../../assets/svgs/Call";
+import Mail from "@/assets/svgs/Mail";
+import Insta from "@/assets/svgs/Insta";
+import Map from "@/assets/svgs/Map";
+import Send from "@/assets/svgs/Send";
 
 export default function Section5() {
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen p-6">
-      <div className="text-center mb-6">
+
+<section className="relative flex flex-col items-center justify-center min-h-screen px-0 py-10 overflow-hidden">
+
+  <div className="absolute left-0 top-1/2 -translate-y-1/2 w-[311px] h-[311px] bg-[radial-gradient(circle,_rgba(68,194,161,0.5)_0%,_transparent_90%)] blur-3xl hidden sm:block"></div>
+
+  <div className="absolute right-0 top-1/3 -translate-y-1/2 w-[311px] h-[311px] md:bg-[radial-gradient(circle,_rgba(68,194,161,0.5)_0%,_transparent_90%)] blur-3xl hidden sm:block"></div>
+
+  <div className="text-center mb-6">
         <h1 className="text-xl font-medium">
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-[#121212] from-primary-n1">
             Reach Us
@@ -23,10 +34,10 @@ export default function Section5() {
       </div>
 
       <div className="max-w-5xl mx-auto">
-  		<div className="bg-white shadow-[inset_0_2px_6px_rgba(109,112,110,0.2)] rounded-3xl overflow-hidden grid grid-cols-1 md:grid-cols-[65%_35%]">
+  		<div className="bg-white shadow-lg rounded-3xl sm:rounded-4xl overflow-hidden grid grid-cols-1 md:grid-cols-[65%_35%] pt-6">
 
-          <div className="p-6 bg-gray-100">
-            <h2 className="text-2xl font-bold text-gray-800">Kirimkan Pesan</h2>
+          <div className="p-6 md:p-8lg:p-10 bg-white">
+            <h2 className="text-xl font-bold text-gray-800">Kirimkan Pesan</h2>
             <form className="mt-4">
               
               <div className="flex flex-col md:flex-row gap-4 mb-3">
@@ -64,15 +75,16 @@ export default function Section5() {
 
               <div className="mb-3">
                 <textarea
-                  className="w-full px-4 py-2 text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
+                  name="message"
+                  className="w-full px-0 py-2 text-gray-900 bg-transparent border-b-2 border-gray-300 focus:border-gray-500 focus:outline-none"
                   placeholder="Apa yang ingin anda sampaikan?"
                 ></textarea>
               </div>
 
               <div className="flex justify-end">
-                <button className="bg-primary-n1 text-white px-6 py-2 rounded-lg flex items-center space-x-2 hover:bg-[#3bb99a]">
+                <button className="bg-primary-n1 text-white px-4 py-2 rounded-lg flex items-center space-x-2 hover:bg-[#3bb99a]">
+                  <Send className="fill-white"/>
                   <span>Kirim</span>
-                  <FontAwesomeIcon icon={faPaperPlane} className="text-white text-sm" />
                 </button>
               </div>
             </form>
@@ -84,31 +96,33 @@ export default function Section5() {
               
               <div className="mt-4 space-y-3">
                 <div className="flex items-center space-x-3">
-                  <FontAwesomeIcon icon={faPhone} className="text-white text-lg" />
+                  <Call className="fill-white" />
                   <p className="text-sm">+62 851-4116-8042</p>
                 </div>
                 <div className="flex items-center space-x-3">
-                  <FontAwesomeIcon icon={faEnvelope} className="text-white text-lg" />
+                  <Mail className="fill-white" />
                   <p className="text-sm">flyhighsinergi.idn@gmail.com</p>
                 </div>
                 <div className="flex items-center space-x-3">
-                  <FontAwesomeIcon icon={faInstagram} className="text-white text-lg" />
+                  <Insta className="fill-white" />
                   <p className="text-sm">flyhighcorp_</p>
                 </div>
                 <div className="flex items-center space-x-3">
-                  <FontAwesomeIcon icon={faMapMarkerAlt} className="text-white text-lg" />
+                  <Map className="fill-white" />
                   <p className="text-sm">Klampis, Surabaya</p>
                 </div>
               </div>
             </div>
 
-            <div className="absolute bottom-4 right-4">
-              <Image src={ev_pic} alt="EV Logo" width={140} height={140} />
+            <div className="absolute bottom-4 right-4 ">
+              <div className="w-15 md:w-36">
+              <Image src={ev_pic} alt="EV Logo" sizes="100%" className="w-full" />
+              </div>
             </div>
-          </div>
-
+          </div>      
         </div>
       </div>
-    </section>
+</section>
+
   );
 }
--- src/components/home/Section6.js
+++ src/components/home/Section6.js
@@ -8,35 +8,38 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 import Image from "next/image";
 import logo from "@/../docs/img/BETULIN.svg";
+import Insta from "@/assets/svgs/Insta";
+import Mail from "@/assets/svgs/Mail";
+import Tiktok from "@/assets/svgs/Tiktok";
 
 export default function Section6() {
 	return (
 	  <footer className="bg-primary-n1 text-white p-6 md:p-8">
 		<div className="container mx-auto grid grid-cols-1 md:grid-cols-[30%_70%] gap-6 md:gap-8">
 		  
-		  <div className="text-center md:text-left mx-auto md:mx-20">
-			<Image src={logo} alt="Logo BETULIN" width={120} height={120} className="mx-auto md:mx-0" />
+		  <div className="text-left mx-auto md:mx-4 lg:mx-20">
+			<Image src={logo} alt="Logo BETULIN" width={120} height={120} className="mx-0" />
 			<p className="text-md md:text-lg mt-2 cursor-default">
 			  Optimalkan performa kendaraan listrik anda dengan perpaduan berbagai fitur BETULIN.
 			</p>
   
-			<div className="flex justify-center md:justify-start space-x-3 mt-4">
+			<div className="flex justify-left md:justify-start space-x-3 mt-4">
 			  <a href="#" className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition">
-				<FontAwesomeIcon icon={faInstagram} className="text-white text-lg" />
+				<Insta className="fill-white" />
 			  </a>
 			  <a href="#" className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition">
-				<FontAwesomeIcon icon={faTiktok} className="text-white text-lg" />
+				<Tiktok className="fill-white"/>
 			  </a>
 			  <a href="#" className="bg-[#48D8B2] w-9 h-9 flex items-center justify-center rounded-md shadow-md hover:bg-[#3bb99a] transition">
-				<FontAwesomeIcon icon={faEnvelope} className="text-white text-lg" />
+				<Mail className="fill-white" />
 			  </a>
 			</div>
 		  </div>
   
-		  <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-			<div className="text-center md:text-left flex flex-col ml-0 md:ml-24">
-			  <h3 className="text-lg md:text-xl font-semibold text-white cursor-default">About</h3>
-			  <ul className="mt-2 space-y-2 md:text-base">
+		  <div className="grid grid-cols-3 md:grid-cols-3 gap-6">
+			<div className="text-center md:text-left flex flex-col ml-0 md:ml-15 lg:ml-24">
+			  <h3 className="text-lg  md:text-xl mt font-semibold text-white cursor-default">About</h3>
+			  <ul className="mt-9 lg:mt-2 md:mt-1.5 space-y-2 md:text-base">
 				<li><a href="#" className="hover:text-white transition">Home</a></li>
 				<li><a href="#" className="hover:text-white transition">About Us</a></li>
 				<li><a href="#" className="hover:text-white transition">Services</a></li>
@@ -44,7 +47,7 @@ export default function Section6() {
 			  </ul>
 			</div>
 
-			<div className="text-center md:text-left flex flex-col">
+			<div className="text-center md:text-left flex flex-col ">
 			  <h3 className="text-lg md:text-xl font-semibold text-white cursor-default">Our Services</h3>
 			  <ul className="mt-2 space-y-2 md:text-base">
 				<li><a href="#" className="hover:text-white transition">Bengkel</a></li>
